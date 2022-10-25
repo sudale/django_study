39d4381f17172b7569e267717ead290868e25527
maengmaengmaeng <dta4718@naver.com>
2022-10-25 16:23:21 +0900

    CRUD 완성

--- myProject01/myapp01/views.py
+++ myProject01/myapp01/views.py
@@ -1,7 +1,13 @@
+from django.http.response import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import redirect, render
+import urllib.parse
+from django.db.models import Q
+import math
 
-from myapp01.models import Board
+from requests import post
+
+from myapp01.models import Board, Comment
 
 # Create your views here.
 UPLOAD_DIR = 'C:\\djangostudy\\upload'
@@ -39,11 +45,86 @@ def insert(request):
     return redirect('/list/')
 
 
-# 전체보기
+# # 전체보기
+# def list(request):
+#     boardList = Board.objects.all()
+#     context = {'boardList': boardList}
+#     return render(request, 'board/list.html', context)
+
+
+# 전체보기 -- 검색
 def list(request):
-    boardList = Board.objects.all()
-    context = {'boardList': boardList}
-    return render(request, 'board/list.html', context)
+    page = request.GET.get('page', 1)
+    word = request.GET.get('word', '')
+    field = request.GET.get('field', 'title')
+
+    # count
+    if field == 'all':
+        boardCount = Board.objects.filter(Q(writer__contains=word) |
+                                          Q(title__contains=word) |
+                                          Q(content__contains=word)).count()
+        # __ like와 같은말
+    elif field == 'writer':
+        boardCount = Board.objects.filter(
+            Q(writer__contains=word)).count()
+
+    elif field == 'title':
+        boardCount = Board.objects.filter(
+            Q(title__contains=word)).count()
+
+    elif field == 'content':
+        boardCount = Board.objects.filter(
+            Q(content__contains=word)).count()
+
+    else:
+        oardList = Board.objects.all().count()
+
+    # 123[다음]    [이전]456[다음]    [이전]78
+
+    pageSize = 5  # 한 화면 게시글 수
+    blockPage = 3  # 보이는 페이지 수
+    currentPage = int(page)
+    start = (currentPage-1)*pageSize
+
+    totPage = math.ceil(boardCount / pageSize)  # 게시글의 전체 페이지수
+    startPage = math.floor((currentPage-1)/blockPage)*blockPage + 1
+    endPage = startPage + blockPage - 1
+
+    if endPage > totPage:
+        endpage = totPage  # endPage = 7
+
+    # 검색내용
+    if field == 'all':
+        boardList = Board.objects.filter(Q(writer__contains=word) |
+                                         Q(title__contains=word) |
+                                         Q(content__contains=word)).order_by('-idx')[start:start+pageSize]
+        # __ like와 같은말
+    elif field == 'writer':
+        boardList = Board.objects.filter(
+            Q(writer__contains=word)).order_by('-idx')[start:start+pageSize]
+
+    elif field == 'title':
+        boardList = Board.objects.filter(
+            Q(title__contains=word)).order_by('-idx')[start:start+pageSize]
+
+    elif field == 'content':
+        boardList = Board.objects.filter(
+            Q(content__contains=word)).order_by('-idx')[start:start+pageSize]
+
+    else:
+        boardList = Board.objects.all().order_by('-idx')[start:start+pageSize]
+
+    context = {'boardList': boardList,
+               'startPage': startPage,
+               'blockPage': blockPage,
+               'endPage': endPage,
+               'totPage': totPage,
+               'boardCount': boardCount,
+               'currentPage': currentPage,
+               'field': field,
+               'word': word,
+               'range': range(startPage, endPage+1)}
+    return render(request, 'board/List.html', context)
 
 
 # 상세보기
@@ -52,7 +133,11 @@ def detail_idx(request):
     dto = Board.objects.get(idx=id)
     dto.hit_up()
     dto.save()
-    return render(request, 'board/detail.html', {'dto': dto})
+
+    # commentList
+    commentList = Comment.objects.filter(board_idx=id).order_by('-idx')
+    return render(request, 'board/detail.html',
+                  {'dto': dto, 'commentList': commentList})
 
 
 # 상세보기(detail/5)
@@ -62,7 +147,9 @@ def detail(request, board_idx):
     dto = Board.objects.get(idx=board_idx)
     dto.hit_up()
     dto.save()
-    return render(request, 'board/detail.html', {'dto': dto})
+
+    commentList = Comment.objects.filter(board_idx=board_idx).order_by('-idx')
+    return render(request, 'board/detail.html', {'dto': dto, 'commentList': commentList})
 
 
 # 수정하기 폼
@@ -110,3 +197,44 @@ def delete(request, board_idx):
     dto = Board.objects.get(idx=board_idx)
     dto.delete()
     return redirect('/list')
+
+
+# 다운로드 횟수
+def download_count(request):
+    id = request.GET['idx']
+    print('id: ', id)
+
+    dto = Board.objects.get(idx=id)
+    dto.down_up()
+    dto.save()
+    count = dto.down
+    print('count: ', count)
+
+    return JsonResponse({'idx': id, 'count': count})
+
+
+# 다운로드
+def download(request):
+    id = request.GET['idx']
+    dto = Board.objects.get(idx=id)
+    path = UPLOAD_DIR+dto.filename
+
+    filename = urllib.parse.quote(dto.filename)
+    print('filename : ', filename)
+    with open(path, 'rb') as file:
+        response = HttpResponse(file.read(),
+                                content_type='application/octet-stream')
+        response['Content-Disposition'] = "attachment;filename*=UTF-8''{0}".format(
+            filename)
+    return response
+
+
+# comment
+@csrf_exempt
+def comment_insert(request):
+    id = request.POST['idx']
+    dto = Comment(board_idx=id,
+                  writer='aa',
+                  content=request.POST['content'])
+    dto.save()
+    return redirect("/detail_idx?idx="+id)
--- myproject02/myproject02/urls.py
+++ myproject02/myproject02/urls.py
@@ -1,4 +1,4 @@
-"""myProject01 URL Configuration
+"""myproject02 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -13,27 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from myapp02 import views
 from django.contrib import admin
 from django.urls import path
-from myapp01 import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', views.list),
-    path('write_form/', views.write_form),
-    path('insert/', views.insert),
-    path('list/', views.list),
-
-    path('detail_idx/', views.detail_idx),
-    path('detail/<int:board_idx>/', views.detail),
-
-    path('update_form/<int:board_idx>/', views.update_form),
-    path('update/', views.update),
-
-    path('delete/<int:board_idx>', views.delete),
-
-    path('download_count/', views.download_count),
-    path('download/', views.download),
-
-    path('comment_insert/', views.comment_insert),
+    path('', views.index),
 ]
